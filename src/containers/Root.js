05605bf814b689dbb82f7fa70a09aa80f9c88731
Michael-Bryant Choa <mbchoa@gmail.com>
2017-03-20 00:54:04 -0700

    Setup webpack, hot loading, HMR

---
+++ src/containers/Root.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Root = () => (
+  <div>Hello from Presence!</div>
+)
+
+export default Root
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    'app': [
+      'react-hot-loader/patch',
+      './src/index'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
+}
