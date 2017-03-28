f8e2a3831fc1785fa293998b0755b2b53a360e13
Michael-Bryant Choa <mbchoa@gmail.com>
2017-03-27 23:06:11 -0700

    Add VRow component

---
+++ src/components/VRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { assign, map } from 'lodash';
+
+const VRow = (props) =>
+    <g transform={ props.transform }>
+        {
+            map(props.children, (child, i) => 
+                React.cloneElement(
+                    child,
+                    assign(
+                        {},
+                        child.props,
+                        { y: i * (+child.props.width + +props.padding)  }
+                    )
+                )
+            )
+        }
+    </g>
+
+export default VRow;
