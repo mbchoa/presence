bd3bd5e0e8ebf422313a4c4099fafb486b87a9a3
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-03 01:44:35 -0700

    Add cors module to server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser'
+import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
 
@@ -15,6 +16,7 @@ mongoose.connect(DB_URI);
 // setup express middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // setup routes
 app.post('/signup', (req, res) => {
