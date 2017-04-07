26800b4ca7f4c6b8ea8d783a5014c3f071b3fe70
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-06 23:22:36 -0700

    Restructure project folder structure

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('postcss-nested'),
+                  require('postcss-triangle')
+                ]
+              }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ]
+}
