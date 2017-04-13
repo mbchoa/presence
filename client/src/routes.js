e6be48d7e416874837c09a7c48b9d7e0fdbfbc69
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-13 15:02:17 -0700

    Add /history/:month and default non-matching route

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,27 +2,32 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import {
     BrowserRouter as Router,
-    Route
+    Route,
+    Switch
 } from 'react-router-dom';
 
+import MonthDetail from './components/pages/History/MonthDetail';
 import HomePage from './components/pages/Home';
 import SignupPage from './components/pages/Signup';
 import LoginPage from './components/pages/Login';
 import StopWatchPage from './components/pages/StopWatch';
 import HistoryPage from './components/pages/History';
 import PrivateRoute from './components/PrivateRoute';
+import NotFoundPage from './components/NotFoundPage';
 
 const Routes = ({ store }) => {
     return (
         <Provider store={ store }>
             <Router>
-                <div>
+                <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/signup" component={SignupPage} />
                     <Route path="/login" component={LoginPage} />
                     <Route path="/stopwatch" component={StopWatchPage} />
-                    <PrivateRoute path="/history" component={HistoryPage} />
-                </div>
+                    <PrivateRoute exact path="/history" component={HistoryPage} />
+                    <PrivateRoute path="/history/:month" component={MonthDetail} />
+                    <Route component={NotFoundPage} />
+                </Switch>
             </Router>
         </Provider>
     );
