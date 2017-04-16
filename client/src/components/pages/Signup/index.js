72b450dbcada0a5d8004ebbd86c3be15d01bfb3a
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-16 00:06:11 -0700

    Connect SignUpPage form to redux

--- client/src/components/pages/Signup/index.js
+++ client/src/components/pages/Signup/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 
-// import { registerUser } from '../../../../redux/actions';
+import { registerUser } from '../../../../redux/actions';
 import styles from './styles.css';
 
 class SignupPage extends Component {
@@ -12,41 +13,38 @@ class SignupPage extends Component {
 
     _submit(data) {
         const { dispatch } = this.props;
-        // dispatch(registerUser(data))
+        dispatch(registerUser(data));
     }
 
     render() {
-        const { handleSubmit } = this.props;
+        const { handleSubmit, signUpError } = this.props;
 
         return (
             <div className="signup">
-                <div className="signup__modal">
-                    <div className="modal">
-                        <header className="signup__header">Welcome back.</header>
-                        <div className="signup__form">
-                            <form onSubmit={ handleSubmit(this._submit) }>
-                                <p className="signup__label">Email</p>
-                                <Field 
-                                    className="signup__input" 
-                                    name="email" 
-                                    component="input" 
-                                    type="text" />
-                                <p className="signup__label">Password</p>
-                                <Field 
-                                    className="signup__input" 
-                                    name="password" 
-                                    component="input" 
-                                    type="password" />
-                                <button type="submit" className="signup__submit">Create</button>
-                            </form>
-                        </div>
-                    </div>
+                <div className="signup__form">
+                    <form onSubmit={ handleSubmit(this._submit) }>
+                        <p className="signup__label">Email</p>
+                        <Field 
+                            className="signup__input" 
+                            name="email" 
+                            component="input" 
+                            type="text" />
+                        <p className="signup__label">Password</p>
+                        <Field 
+                            className="signup__input" 
+                            name="password" 
+                            component="input" 
+                            type="password" />
+                        <button type="submit" className="signup__submit">Create</button>
+                        { signUpError && <p className="signup__error">{ signUpError }</p> }
+                    </form>
                 </div>
             </div>
         );
     }
 }
 
-export default reduxForm({
-    form: 'signup',
-})(SignupPage);
+export default connect(
+    ({ root }) => ({ signUpError: root.signUpError }),
+    {}
+)(reduxForm({ form: 'signup' })(SignupPage));
