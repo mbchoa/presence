f26694ad965442fdb46876c3cdae115b175234ba
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-18 09:08:47 -0700

    Stop timer and save stopwatch time before unmonting stopwatch page

--- client/src/components/pages/StopWatch/index.js
+++ client/src/components/pages/StopWatch/index.js
@@ -51,6 +51,13 @@ class StopWatchPage extends Component {
         this.setState({ elapsedMs: this.props.currentSessionTime });
     }
     
+    componentWillUnmount() {
+        const { saveTime } = this.props;
+
+        this.timer.stop();
+        saveTime(this.state.elapsedMs);
+    }
+
     render() {
         const { isStarted } = this.timer;
         const { elapsedMs } = this.state;
