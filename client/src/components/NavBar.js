fe88f534341eabeff89313ae046a2be48043833f
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-18 23:42:10 -0700

    Fix React missing key iterator warning

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,24 +6,24 @@ import { logoutUser } from '../../redux/actions';
 
 const NavBar = ({ isAuthenticated, logoutUser }) => (
     <ul className="navbar">
-        <li className="navbar__link"><Link to="/">Home</Link></li>
+        <li key={0} className="navbar__link"><Link to="/">Home</Link></li>
         { isAuthenticated &&
             [
-                <li className="navbar__link"><Link to="/history">History</Link></li>,
-                <li className="navbar__link"><Link to="/history/april">April</Link></li>,
-                <li className="navbar__link"><Link to="/stopwatch">Stopwatch</Link></li>
+                <li key={1} className="navbar__link"><Link to="/history">History</Link></li>,
+                <li key={2} className="navbar__link"><Link to="/history/april">April</Link></li>,
+                <li key={3} className="navbar__link"><Link to="/stopwatch">Stopwatch</Link></li>
             ]
         }
         { !isAuthenticated && 
             [
-                <li className="navbar__link"><Link to="/signup">Signup</Link></li>,
-                <li className="navbar__link">
+                <li key={4} className="navbar__link"><Link to="/signup">Signup</Link></li>,
+                <li key={5} className="navbar__link">
                     <Link to="/login">Login</Link>
                 </li> 
             ]
         }
         { isAuthenticated && 
-            <li className="navbar__link">
+            <li key={6} className="navbar__link">
                 <button onClick={ logoutUser }>Logout</button>
             </li> 
         }
