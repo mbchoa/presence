cf3e66b6478629b137645d64ced430076e7a1636
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-19 02:06:23 -0700

    Update NavBar to dynamically set current month link

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 import { logoutUser } from '../../redux/actions';
 
-const NavBar = ({ isAuthenticated, logoutUser }) => (
-    <ul className="navbar">
-        <li key={0} className="navbar__link"><Link to="/">Home</Link></li>
-        { isAuthenticated &&
-            [
-                <li key={1} className="navbar__link"><Link to="/history">History</Link></li>,
-                <li key={2} className="navbar__link"><Link to="/history/april">April</Link></li>,
-                <li key={3} className="navbar__link"><Link to="/stopwatch">Stopwatch</Link></li>
-            ]
-        }
-        { !isAuthenticated && 
-            [
-                <li key={4} className="navbar__link"><Link to="/signup">Signup</Link></li>,
-                <li key={5} className="navbar__link">
-                    <Link to="/login">Login</Link>
+const NavBar = ({ isAuthenticated, logoutUser }) => {
+    const currentMonth = format(new Date(), 'MMMM');
+
+    return (
+        <ul className="navbar">
+            <li key={0} className="navbar__link"><Link to="/">Home</Link></li>
+            { isAuthenticated &&
+                [
+                    <li key={1} className="navbar__link"><Link to="/history">History</Link></li>,
+                    <li key={2} className="navbar__link">
+                        <Link to={`/history/${currentMonth}`}>{ currentMonth }</Link>
+                    </li>,
+                    <li key={3} className="navbar__link"><Link to="/stopwatch">Stopwatch</Link></li>
+                ]
+            }
+            { !isAuthenticated && 
+                [
+                    <li key={4} className="navbar__link"><Link to="/signup">Signup</Link></li>,
+                    <li key={5} className="navbar__link">
+                        <Link to="/login">Login</Link>
+                    </li> 
+                ]
+            }
+            { isAuthenticated && 
+                <li key={6} className="navbar__link">
+                    <button onClick={ logoutUser }>Logout</button>
                 </li> 
-            ]
-        }
-        { isAuthenticated && 
-            <li key={6} className="navbar__link">
-                <button onClick={ logoutUser }>Logout</button>
-            </li> 
-        }
-    </ul>
-);
+            }
+        </ul>
+    );
+};
 
 export default connect(
     ({ root }) => ({ isAuthenticated: root.isAuthenticated }),
