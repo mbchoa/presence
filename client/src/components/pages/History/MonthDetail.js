4a4c7c7d3d516192c0378a4ef16fce19e7496362
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-19 23:52:07 -0700

    Deprecate Loading component, convert MonthDetail to stateful component

--- client/src/components/pages/History/MonthDetail.js
+++ client/src/components/pages/History/MonthDetail.js
@@ -1,47 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { partial } from 'lodash';
 
 import { getMonthSessions } from '../../../../redux/actions';
 import Loading from '../../Loading';
 
-const MonthDetail = ({ getMonthSessions, match }) => (
-    <div className="month-detail">
-        <h1 className="month-detail__title">{ match.params.month } 2017</h1>
-        <Loading { ...{ 
-            getMonthSessions: partial(getMonthSessions, match.params.month) 
-        } }>
-            <h3 className="month-detail__month-total">Month Total</h3>
-            <label>3 hours 4 minutes</label>
-            <ul className="month-detail__days">
-                <li className="month-detail__day">
-                    <h4>March 1</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-                <li className="month-detail__day">
-                    <h4>March 2</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-                <li className="month-detail__day">
-                    <h4>March 3</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-                <li className="month-detail__day">
-                    <h4>March 5</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-                <li className="month-detail__day">
-                    <h4>March 6</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-                <li className="month-detail__day">
-                    <h4>March 8</h4>
-                    <div>3 hours 2 minutes </div>
-                </li>
-            </ul>
-        </Loading>
-    </div>
-);
+class MonthDetail extends Component {
+    state = {
+        isLoading: false
+    };
+
+    render () {
+        const { match } = this.props;
+        const { isLoading } = this.state;
+
+        return (
+            <div className="month-detail">
+                <h1 className="month-detail__title">{ match.params.month } 2017</h1>
+                { isLoading
+                    ? <h3>Loading...</h3>
+                    : <div>
+                        <h3 className="month-detail__month-total">Month Total</h3>
+                        <label>3 hours 4 minutes</label>
+                        <ul className="month-detail__days">
+                            <li className="month-detail__day">
+                                <h4>March 1</h4>
+                                <div>3 hours 2 minutes </div>
+                            </li>
+                            <li className="month-detail__day">
+                                <h4>March 2</h4>
+                                <div>3 hours 2 minutes </div>
+                            </li>
+                        </ul>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
 
 export default connect(
     null,
