e69eb593bafe76824ebfefd014216a33aa1d59d0
Michael-Bryant Choa <mbchoa@gmail.com>
2017-04-30 12:49:09 -0700

    Move config.js file to root and added additional env variables

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    BACKEND_PORT: process.env.BACKEND_PORT || 3000,
+    DB_URI: process.env.DB_URI || 'mongodb://localhost:27017/presence',
+    FRONTEND_HOST: process.env.FRONTEND_HOST || 'localhost',
+    FRONTEND_PORT: process.env.FRONTEND_PORT || 8080,
+    REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
+    SESSION_SECRET: process.env.SESSION_SECRET || 'cowgomoo',
+}
