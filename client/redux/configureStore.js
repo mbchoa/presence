df179151124c42ff478edf089cab1bfabe042e91
Michael-Bryant Choa <mbchoa@gmail.com>
2017-05-06 13:51:05 -0700

    Add Redux Devtool compose enhancer to configureStore

--- client/redux/configureStore.js
+++ client/redux/configureStore.js
@@ -1,4 +1,9 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { 
+    applyMiddleware, 
+    combineReducers, 
+    compose, 
+    createStore 
+} from 'redux';
 import { reducer as form } from 'redux-form';
 import thunkMiddleware from 'redux-thunk';
 
@@ -6,13 +11,13 @@ import root from './reducers';
 import initial from './initialState';
 
 export default function configureStore(initialState) {
-
+    const composeEnhancers = __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
         combineReducers({
             form,
             root,
         }),
         { ...initial, ...initialState },
-        applyMiddleware(thunkMiddleware),
+        composeEnhancers(applyMiddleware(thunkMiddleware))
     );
 }
