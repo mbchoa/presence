4cd34fed982eef9664f692a3ff33ff7a3381f00e
Michael-Bryant Choa <mbchoa@gmail.com>
2017-05-07 01:19:27 -0700

    Wire up LoginForm component to Redux

--- client/src/components/pages/Login/LoginForm.js
+++ client/src/components/pages/Login/LoginForm.js
@@ -1,45 +1,63 @@
 import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { TextField } from 'redux-form-material-ui';
 import RaisedButton from 'material-ui/RaisedButton';
 import { blueGrey600 } from 'material-ui/styles/colors';
 
+import { loginUser } from '../../../../redux/actions';
+
 class LoginForm extends Component {
+    constructor () {
+        super();
+        this.renderTextField = this.renderTextField.bind(this);
+        this._submit = this._submit.bind(this);
+    }
+
+    _submit (data) {
+        const { loginUser } = this.props;
+        loginUser(data);
+    }
+
+    renderTextField ({ input, label }) {
+        return (
+            <TextField {...{
+                floatingLabelText: label,
+                floatingLabelFocusStyle: {
+                    color: blueGrey600
+                },
+                fullWidth: true,
+                ...input,
+                inputStyle: {
+                    color: 'black'
+                },
+                underlineFocusStyle: {
+                    borderColor: blueGrey600
+                }
+            }} />
+        );
+    }
+
     render () {
+        const { handleSubmit } = this.props;
         return (
             <div className="modal__form">
-                <TextField {...{
-                    floatingLabelText: 'Email',
-                    floatingLabelFocusStyle: {
-                        color: blueGrey600
-                    },
-                    fullWidth: true,
-                    inputStyle: {
-                        color: 'black'
-                    },
-                    underlineFocusStyle: {
-                        borderColor: blueGrey600
-                    }
-                }} />
-                <TextField {...{
-                    floatingLabelText: 'Password',
-                    floatingLabelFocusStyle: {
-                        color: blueGrey600
-                    },
-                    fullWidth: true,
-                    inputStyle: {
-                        color: 'black'
-                    },
-                    type: 'password',
-                    underlineFocusStyle: {
-                        borderColor: blueGrey600
-                    }
-                }} />
-                <RaisedButton 
-                    label="Login"
-                    fullWidth={true} />
+                <form onSubmit={ handleSubmit(this._submit) }>
+                    <Field name="email" label="Email" component={this.renderTextField} />
+                    <Field name="password" label="Password" component={this.renderTextField} />
+                    <RaisedButton 
+                        label="Login"
+                        fullWidth={true} 
+                        type="submit" />
+                </form>
             </div>
         );
     }
 }
 
-export default LoginForm;
+ export default connect(
+     null,
+     { loginUser }
+ )(reduxForm({
+     form: 'login',
+ })(LoginForm))
--- client/src/components/pages/Login/index.js
+++ client/src/components/pages/Login/index.js
@@ -1,41 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
 import { Redirect } from 'react-router-dom';
 import { Toolbar, ToolbarTitle } from 'material-ui/Toolbar';
 import Paper from 'material-ui/Paper';
-
-import { loginUser } from '../../../../redux/actions';
 
 import styles from './styles.css';
 
 import LoginForm from './LoginForm';
 
-class LoginPage extends Component {
-    constructor () {
-        super();
-        this._submit = this._submit.bind(this);
-    }
-
-    _submit (data) {
-        const { dispatch } = this.props;
-        dispatch(loginUser(data));
-    }
-
-    render () {
-        const { 
-            handleSubmit, 
-            isAuthenticated, 
-            loginErrorMessage 
-        } = this.props;
-        const { from } = this.props.location.state || { from: { pathname: '/' } };
-
-        if (isAuthenticated) {  
-            return <Redirect to={ from } />;
-        }
-
-        return (
-            <div className="login">
+const LoginPage = ({ isAuthenticated, location }) => {
+    const { from } = location.state || { from: { pathname: '/' } };
+    return isAuthenticated
+        ? <Redirect t={ from } />
+        : <div className="login">
                 <div className="login__modal">
                     <Paper className="modal">
                         <Toolbar>
@@ -47,15 +24,10 @@ class LoginPage extends Component {
                     </Paper>
                 </div>
             </div>
-        );
-    }
-}
+};
 
 export default connect(
     ({ root }) => ({ 
-        isAuthenticated: root.isAuthenticated,
-        loginErrorMessage: root.loginErrorMessage
+        isAuthenticated: root.isAuthenticated
     })
-)(reduxForm({
-    form: 'login',
-})(LoginPage));
+)(LoginPage);
