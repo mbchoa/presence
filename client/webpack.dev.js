5d1b7eb6c934d844f46cae4dd7545bdb34a34a9a
Michael-Bryant Choa <mbchoa@gmail.com>
2017-05-08 00:46:27 -0700

    Switch to cheap-module-eval-sourcemap for source maps

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -2,6 +2,7 @@ const WebpackMerge = require('webpack-merge');
 const CommonConfig = require('./webpack.common.js');
 
 module.exports = WebpackMerge(CommonConfig, {
+  devtool: 'cheap-module-eval-source-map',
   entry: {
     'app': [
       'react-hot-loader/patch',
