d8e52824ffee8ae1b6fc656e9d97f39e9dbfae72
Michael-Bryant Choa <mbchoa@gmail.com>
2017-05-09 01:17:44 -0700

    Add key prop to Week component

--- client/src/components/pages/History/Week.js
+++ client/src/components/pages/History/Week.js
@@ -4,11 +4,11 @@ import { map } from 'lodash';
 import VRow from '../../VRow';
 import Square from '../../Square';
 
-const Week = ({ data }) => {
+const Week = ({ data, ...props }) => {
     return (
-        <VRow padding="5">
-            { map(data, dayData => 
-                <Square width="50" height="50" data={dayData} />
+        <VRow {...props}>
+            { map(data, (dayData, key) => 
+                <Square key={key} width="50" height="50" data={dayData} />
             )}
         </VRow>
     );
