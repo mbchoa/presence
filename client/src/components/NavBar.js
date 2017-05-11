fe9a79a846895de717d2e7f284f70dbca87f01c5
Michael-Bryant Choa <mbchoa@gmail.com>
2017-05-10 23:53:58 -0700

    Hide Sign Up button when on signup page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -14,67 +14,55 @@ import { logoutUser } from '../../redux/actions';
 
 import FlatButtonLink from './FlatButtonLink';
 
-class NavBar extends Component {
-    static defaultProps = {
-        currentMonth: format(new Date(), 'MMMM')
-    };
-
-    render () {
-        const {
-            currentMonth,
-            isAuthenticated,
-            location: { pathname },
-            logoutUser
-        } = this.props;
-
-        return (
-            <Toolbar>
-                <Link to="/" style={{ textDecoration: 'none' }}>
-                    <ToolbarTitle text="Presence" style={{
-                        color: white
-                    }} />
-                </Link>
-                <ToolbarGroup>
-                    { isAuthenticated && [
-                        <FlatButtonLink
-                            key="stopwatch"
-                            label="Stopwatch"
-                            to="/stopwatch"
-                        />,
-                        <FlatButtonLink 
-                            key="history"
-                            label={ currentMonth }
-                            to={ `/history/${currentMonth}` }
-                        />,
-                        <FlatButton
-                            key="logout"
-                            label="Logout"
-                            onClick={ logoutUser }
-                            style={{
-                                marginLeft: '0px',
-                                marginRight: '0px'
-                            }}
-                        />
-                    ]}
-                    { !isAuthenticated &&
-                        <FlatButtonLink 
-                            key="signup" 
-                            label="Signup" 
-                            to="/signup" 
-                        />
-                    }
-                    { pathname !== '/login' && !isAuthenticated &&
-                        <FlatButtonLink 
-                            key="login"
-                            label="Login" 
-                            to="/login" 
-                        />
-                    }
-                </ToolbarGroup>
-            </Toolbar>
-        );
-    }
-}
+const NavBar = ({
+    isAuthenticated,
+    location: { pathname },
+    logoutUser
+}) => 
+    <Toolbar>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+            <ToolbarTitle text="Presence" style={{
+                color: white
+            }} />
+        </Link>
+        <ToolbarGroup>
+            { isAuthenticated && [
+                <FlatButtonLink
+                    key="stopwatch"
+                    label="Stopwatch"
+                    to="/stopwatch"
+                />,
+                <FlatButtonLink 
+                    key="history"
+                    label="History"
+                    to="/history"
+                />,
+                <FlatButton
+                    key="logout"
+                    label="Logout"
+                    onClick={ logoutUser }
+                    style={{
+                        marginLeft: '0px',
+                        marginRight: '0px'
+                    }}
+                />
+            ]}
+            { pathname !== '/signup' && !isAuthenticated &&
+                <FlatButtonLink 
+                    key="signup" 
+                    label="Signup" 
+                    to="/signup" 
+                />
+            }
+            { pathname !== '/login' && !isAuthenticated &&
+                <FlatButtonLink 
+                    key="login"
+                    label="Login" 
+                    to="/login" 
+                />
+            }
+        </ToolbarGroup>
+    </Toolbar>
 
 export default withRouter(connect(
     ({ root }) => ({ isAuthenticated: root.isAuthenticated }),
